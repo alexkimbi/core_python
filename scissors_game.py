4a0a0c811f53a19c8d3a5abaffce9559cc4fd2af
Alex Kimbi <alexkimbi@hotmail.com>
2023-10-04 12:07:00 -0400

    Added new game files

--- scissors_game.py
+++ scissors_game.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-computer_choice = input("What is the computer's choice? ")
-user_choice = input("What is your choice? ")
+import random
+
+computer_choice = random.choice(['rock','paper','scissors']) # This is a list where  the random will choose from
+user_choice = input('Do you want rock,paper or scissors?')
+print('Computer Choice:', computer_choice)
+
+###user_choice = input("What is your choice? ")
 if computer_choice == 'paper' and user_choice == 'scissors':
     print('You won the game')
 elif computer_choice == 'scissors' and user_choice == 'paper':
@@ -11,3 +16,4 @@ elif computer_choice == 'rock' and user_choice == 'paper':
     print('You won the game')
 else:
     print('All your choices are wrong ... BYE BYE')
+    
