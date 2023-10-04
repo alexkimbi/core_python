1bdd07c3c2eb2fcf34d5867c5af402b31e957c58
Alex Kimbi <alexkimbi@hotmail.com>
2023-10-04 10:56:31 -0400

    added more

--- scissors_game.py
+++ scissors_game.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 computer_choice = input("What is the computer's choice? ")
 user_choice = input("What is your choice? ")
-if computer_choice == "paper" and user_choice == "scissors":
-    print("Your won the game")
-elif:
-    if computer_choice == "scissors" and user_choice == "paper":
-    print("Your won the game")
-elif:
-    if computer_choice == "scissors" and user_choice == "paper":
-    print("Your won the game")
+if computer_choice == 'paper' and user_choice == 'scissors':
+    print('You won the game')
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    print('You won the game')
+elif computer_choice == 'paper' and user_choice == 'rock':
+    print('You won the game')
+elif computer_choice == 'rock' and user_choice == 'paper':
+    print('You won the game')
+else:
+    print('All your choices arewrong ... BYE BYE')
