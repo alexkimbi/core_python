7b13b242a1b0f941013e0dc0fef5ed08591fa2d5
Alex Kimbi <alexkimbi@hotmail.com>
2023-11-01 14:12:16 -0400

    weather

---
+++ weather.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+forecast = input("What is the weather like today? \n")
+if forecast == "rainy":
+    print("Stay home and stay warm")
+else:
+    print("You are free to go out and enjoy yourself!")
+    print("...BYE BYE")
+    
