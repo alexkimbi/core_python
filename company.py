852cb76fa52c75d527e68b8859d2513b59e2f85e
Alex Kimbi <alexkimbi@hotmail.com>
2023-11-30 08:59:33 -0600

    changes

--- company.py
+++ company.py
@@ -3,7 +3,6 @@ class Company:
     def __init__(self):
         self.employees = []
         self.top_employees = {}
-        # self.employees_years = {}
     def add_employee(self, new_employee):
         # Expects employee's fname, lname and salary 
         self.employees.append(new_employee)
@@ -28,15 +27,13 @@ class Company:
         print(self.top_employees)
         
 # Assignment to add employees salary with 2 or more years of service by 5%
-    def employees_years(self):
-        for i in self.employees:
-            print(i.lname, i.fname, i.salary, i.years)
-            if i.years > 2:
-                print(i.lname)
-            # (0.05 * i.calculate_paycheck() + i.calculate_paycheck())
-        print(self.employees_years)             
-     
-
+    # def years_of_service(self):
+    #     print('The years of empoyment:')
+    #     for i in self.employees:
+    #         # if i.years() > 1:
+    #         print(i.years)
+    #         self.years[i.fname, i.lname()]
+    #     print(self.years_of_service)
                  
         
 def main():
@@ -48,11 +45,10 @@ def main():
     my_company.add_employee(employee2)
     employee3 = Employee('Bob', 'Brown', 250000, 2 )
     my_company.add_employee(employee3)
-    
+   
+    # my_company.years_of_service()
     my_company.top_pay_employees()
-    my_company.employees_years() 
    # my_company.display_employees()
     my_company.pay_employees()
-    
-    
+  
 main()
